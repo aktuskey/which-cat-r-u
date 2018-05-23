849c20a66bd485f15abe825de188218098a3b520
Alexa <alexa.tuskey@gmail.com>
2018-05-22 20:25:43 -0500

    Added quiz tile

--- elm/Main.elm
+++ elm/Main.elm
@@ -7,15 +7,25 @@ import Html.Attributes exposing (..)
 main : Html msg
 main =
     div [ class "page" ]
-        [ h1 [ class "page__heading-h1" ] [ text "Which Cat Are You?!" ]
-        , h3 [ class "subtitle" ] [ text "We all know you're dying to know." ]
-        , p [ class "body" ] [ text "Quizzes for cats!" ]
+        [ h1 [ class "page__heading-h1" ] [ text "Catfeed Quizzes" ]
+        , h3 [ class "page__heading-h2" ] [ text "All Quizzes. All About Cats." ]
+        , p [ class "body" ] [ text "Select a quiz to begin!" ]
         , ourLink "http://www.google.com" "Click Me!"
         , ourLink "http://www.thedodo.com" "More Cute Cats!"
         , ourLink "http://www.elm-lang.org" "Built with Love on Elm"
+        , quiz "http://www.google.com" "Which Cat Are You?" "/"
         ]
 
 
 ourLink url label =
     div []
         [ a [ class "page__link", href url ] [ text label ] ]
+
+
+quiz quizUrl quizTitle imgUrl =
+    div [ class "quiz-tile" ]
+        [ a [ href quizUrl ]
+            [ img [ class "quiz-tile__img", src imgUrl ] []
+            , h3 [ class "quiz-tile__title" ] [ text quizTitle ]
+            ]
+        ]
