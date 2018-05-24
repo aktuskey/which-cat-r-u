030b0b1b31b82c1f4d6f5a97507394a63a089832
Alexa <alexa.tuskey@gmail.com>
2018-05-23 21:30:01 -0500

    Adding lotsa colors

--- elm/Main.elm
+++ elm/Main.elm
@@ -6,14 +6,14 @@ import Html.Attributes exposing (..)
 
 main : Html msg
 main =
-    div [ class "page" ]
+    div [ class "page page__home-bg" ]
         [ h1 [ class "page__heading-h1" ] [ text "Catfeed Quizzes" ]
         , h3 [ class "page__heading-h2" ] [ text "All Quizzes. All About Cats." ]
         , p [ class "body" ] [ text "Select a quiz to begin!" ]
-        , ourLink "http://www.google.com" "Click Me!"
+        , ourLink "http://www.google.com" "Click Me!!"
         , ourLink "http://www.thedodo.com" "More Cute Cats!"
-        , ourLink "http://www.elm-lang.org" "Built with Love on Elm"
-        , quiz "http://www.google.com" "Which Cat Are You?" "/"
+        , quiz "http://www.google.com" "Which Cat Are You?"
+        , footer "© 2018. Plz don't steal." "https://twitter.com/LaserTuskey" "http://www.elm-lang.org" "https://www.ruby-lang.org/en/"
         ]
 
 
@@ -22,10 +22,35 @@ ourLink url label =
         [ a [ class "page__link", href url ] [ text label ] ]
 
 
-quiz quizUrl quizTitle imgUrl =
+quiz quizUrl quizTitle =
     div [ class "quiz-tile" ]
         [ a [ href quizUrl ]
-            [ img [ class "quiz-tile__img", src imgUrl ] []
+            [ div [ class "quiz-tile__img", style [ ( "background-image", "url(images/flower-kitten.jpg)" ) ] ] []
             , h3 [ class "quiz-tile__title" ] [ text quizTitle ]
             ]
+        ]
+
+
+footer copyright twitterUrl elmUrl rubyUrl =
+    div [ class "footer" ]
+        [ span [ class "footer__copyright" ]
+            [ text copyright ]
+        , span
+            [ class "footer__text" ]
+            [ text "Built with ♥ by " ]
+        , a
+            [ class "footer__link", href twitterUrl ]
+            [ text "@LaserTuskey" ]
+        , span
+            [ class "footer__text" ]
+            [ text " on " ]
+        , a
+            [ class "footer__link", href elmUrl ]
+            [ text "Elm" ]
+        , span
+            [ class "footer__text" ]
+            [ text " + " ]
+        , a
+            [ class "footer__link", href rubyUrl ]
+            [ text "Ruby" ]
         ]
