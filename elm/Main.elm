5b4bba2da22f554eca3117923e6f402272e7384f
Alexa <alexa.tuskey@gmail.com>
2018-06-26 19:59:48 -0500

    Tightening up code

--- elm/Main.elm
+++ elm/Main.elm
@@ -4,16 +4,68 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+type alias QuizInfo =
+    { title : String
+    , imgUrl : String
+    , quizUrl : String
+    , description : String
+    }
+
+
+quizzes : List QuizInfo
+quizzes =
+    [ QuizInfo
+        "Which flower crown matches your purrsonality?"
+        "/images/flower-kitten.jpg"
+        "/"
+        "Lorem ipsum blah blah blah"
+    , QuizInfo
+        "Which celebrity cat are you most like?"
+        "/images/LilBubChristmas.jpg"
+        "/"
+        "Lorem ipsum dsjhfalksjdhfl"
+    , QuizInfo
+        "We bet we can guess your ultimate jam"
+        "/images/wo05xcp.jpg"
+        "/"
+        "akjhs aksdhakljsdhalkj aksjdhjh"
+    , QuizInfo
+        "Buy some fancy collars from Petco and we’ll give you your spirit cat"
+        "/images/Turquoise-cat-necklace-from-TrendyKitty-on-Etsy-13.80.jpg"
+        "/"
+        "al alskdja akjwihao asks"
+    , QuizInfo
+        "Which dog should be your new BFF?"
+        "/images/370135.jpg"
+        "/"
+        "ak akjshfi qiyr kasjdhfl"
+    , QuizInfo
+        "Which badass fictional kitty are you?"
+        "/images/phantasms3.jpg"
+        "/"
+        "mnb asdp laksdjhflaks diseoriuaosidjf askjfh"
+    ]
+
+
 main : Html msg
 main =
     div [ class "page page__home-bg" ]
-        [ h1 [ class "page__heading-h1" ] [ text "Catfeed Quizzes" ]
-        , h3 [ class "page__heading-h2" ] [ text "All Quizzes. All About Cats." ]
-        , p [ class "body" ] [ text "Select a quiz to begin!" ]
-        , ourLink "http://www.google.com" "Click Me!!"
-        , ourLink "http://www.thedodo.com" "More Cute Cats!"
-        , div [ class "quiz-tiles" ] [ quiz "http://www.google.com" "Which flower crown matches your purrsonality?" ]
+        [ navbar "Catfeed Quizzes" "All Quizzes. All About Cats." "/images/cheezburger.svg"
+        , div [ class "quiz-tiles" ]
+            (List.map quiz quizzes)
         , footer "© 2018. Plz don't steal." "https://twitter.com/LaserTuskey" "http://www.elm-lang.org" "https://www.ruby-lang.org/en/"
+        ]
+
+
+navbar title subtitle menuIconUrl =
+    div [ class "navbar" ]
+        [ div [ class "navbar__brand" ]
+            [ h1 [ class "page__heading-h1" ] [ text title ]
+            , h3 [ class "page__heading-h3" ] [ text subtitle ]
+            ]
+        , div [ class "navbar__icons" ]
+            [ button [ class "icon icon__cheezburger navbar__menu" ] []
+            ]
         ]
 
 
@@ -22,19 +74,20 @@ ourLink url label =
         [ a [ class "page__link", href url ] [ text label ] ]
 
 
-quiz quizUrl quizTitle =
+quiz : QuizInfo -> Html msg
+quiz info =
     div [ class "quiz-tile" ]
-        [ a [ href quizUrl ]
+        [ a [ href info.quizUrl, title info.description ]
             [ div
                 [ class "quiz-tile__img"
                 , style
                     [ ( "background-image"
-                      , "url(images/flower-kitten.jpg)"
+                      , "url(" ++ info.imgUrl ++ ")"
                       )
                     ]
                 ]
                 []
-            , h3 [ class "quiz-tile__title" ] [ text quizTitle ]
+            , h3 [ class "quiz-tile__title" ] [ text info.title ]
             ]
         ]
 
