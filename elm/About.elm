3a1d396ea8017205f040cb80a0a92b9bc7a4e092
Alexa <alexa.tuskey@gmail.com>
2018-07-17 09:12:01 -0500

    FIxed about page Elm routing issue

--- elm/About.elm
+++ elm/About.elm
@@ -4,7 +4,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
-about : Html msg
-about =
+main : Html msg
+main =
     div [ class "page page__home-bg" ]
-        [ h1 [ class "page__heading-h1" ] [ text "Hello World!" ] ]
+        [ h1 [ class "page__heading-h1" ] [ text "Hello Ryan is cool!" ] ]
